5f1e8ac99ef522551dbaf0b142842f2e4185303f
Alexander Pape <a.pape@paneon.de>
2016-03-17 11:22:59 +0100

    Update grunt and add a demo sass file

--- gruntfile.js
+++ gruntfile.js
@@ -7,25 +7,26 @@ module.exports = function(grunt) {
 
     var pkg = grunt.file.readJSON("package.json");
 
-    // New entry files should be listed here.
-    // each file will get compass'ed ( _sass/[filename].scss => mdeTemplates/css/[filename].css )
-    // and for production minified ( mdeTemplates/css/[filename].css => mdeTemplates/css/[filename].min.css )
     var sassEntryFiles = [
-        { filename: "master", title: "MDE Master" },
-        { filename: "mde", title: "MDE" },
+        { filename: "main", title: "Main" }
     ];
 
     var paths = {
 
-        sass: path.join("_sass"),
-        images: path.join("_sass", "images"),
+        sass: path.join("sass"),
+        images: path.join("sass", "images"),
         js: path.join("js"),
 
+        dev: {
+            js: path.join("dev", "js"),
+            css: path.join("dev", "css"),
+            images: path.join("dev", "css", "images")
+        },
 
-        build: {
-            js: path.join("mdeTemplates", "js"),
-            css: path.join("mdeTemplates", "css"),
-            images: path.join("mdeTemplates", "css", "images")
+        dist: {
+            js: path.join("dist", "js"),
+            css: path.join("dist", "css"),
+            images: path.join("dist", "css", "images")
         }
 
     };
@@ -33,21 +34,12 @@ module.exports = function(grunt) {
     var styleFiles = {
         // Source files
         config: path.resolve(paths.sass, "config.rb"),
-        //master: path.resolve(paths.sass, "master.scss"),
-        //mde: path.resolve(paths.sass, "mde.scss"),
-        //kino: path.resolve(paths.sass, "app.scss"),
 
-        // Unminified compass generated files
-        dev: {
-            //master: path.relative(paths.build.css, "master.css"),
-            //mde: path.relative(paths.build.css, "mde.css")
-        },
+        // Generated files for development
+        dev: { },
 
-        // Production paths
-        build: {
-            //master: path.resolve(paths.build.css, "master.min.css"),
-            //mde: path.resolve(paths.build.css, "mde.min.css")
-        }
+        // Production build paths
+        dist: {  }
     };
 
     // Include every entry of sassEntryFiles into the Lists
@@ -55,119 +47,38 @@ module.exports = function(grunt) {
     for(i = 0; i < sassEntryFiles.length; i++){
         filename = sassEntryFiles[i].filename;
         styleFiles[filename] = path.resolve(paths.sass, filename+".scss");
-        styleFiles.dev[filename] = path.resolve(paths.build.css, filename+".css");
-        styleFiles.build[filename] = path.resolve(paths.build.css, filename+".min.css");
+        styleFiles.dev[filename] = path.resolve(paths.dev.css, filename+".css");
+        styleFiles.dist[filename] = path.resolve(paths.dist.css, filename+".min.css");
     }
-
 
     var jsFiles = {
         script: path.join(paths.js, "app.tsx"),
-        search: path.join(paths.js, "standalone_search.ts"),
 
-        build: {
-            scriptDev: path.join(paths.build.js, "script.js"),
+        dev: {
+            script: path.join(paths.build.js, "script.js"),
+            vendor: path.join(paths.build.js, "vendor.js")
+        },
+
+        dist: {
             script: path.join(paths.build.js, "script.min.js"),
-            searchDev: path.join(paths.build.js, "standalone_search.js"),
-            search: path.join(paths.build.js, "standalone_search.min.js"),
             vendor: path.join(paths.build.js, "vendor.min.js")
         }
     };
 
-    var getScriptFiles = function(){
+    var getVendorFiles = function(path){
         var config = {};
 
-        config[jsFiles.build.script] = jsFiles.build.scriptDev;
-
-        return config;
-    };
-
-    var getVendorFiles = function(){
-        var config = {};
-
-        config[jsFiles.build.vendor] = [
-            // Basic Libraries
-            "node_modules/npm-modernizr/modernizr.js",
-            "node_modules/webcomponents.js/webcomponents.js",                       // Web components polyfill (load before jquery)
-            "node_modules/jquery/dist/jquery.min.js",
-            "node_modules/scriptjs/dist/script.min.js",
-
-            // Common Libraries
-            "node_modules/lazyloadxt/dist/jquery.lazyloadxt.min.js",                // lazy loading
-            "node_modules/slick-carousel/slick/slick.min.js",                       // slider
-            "node_modules/lodash/lodash.min.js", // lodash
-
-            // Potentially async libraries (later)
-            "node_modules/headroom.js/dist/headroom.min.js",                        // sticky Header
-            "node_modules/jquery-match-height/dist/jquery.matchHeight-min.js",      // match heights of cols
-            "node_modules/raty-js/lib/jquery.raty.js",                              // ratings stars
+        config[path] = [
             "node_modules/react/dist/react.min.js",                                     // React
-            "node_modules/react-dom/dist/react-dom.min.js",
-            "node_modules/sidr/dist/jquery.sidr.min.js",                            // side menu
-            "node_modules/shariff/build/shariff.min.js",                            // social sharing
-            "node_modules/jquery-datetimepicker/jquery.datetimepicker.js",          // date/time picker
-            "node_modules/devbridge-autocomplete/dist/jquery.autocomplete.min.js"   // autocomplete (temporary)
+            "node_modules/react-dom/dist/react-dom.min.js"
         ];
 
         return config;
     };
 
-    var bannerTemplate = function(options){
-
-        options = options || {};
-
-        var name = options.name || "MDE";
-        var version = " v"+options.version || "";
-        var css = options.css || false;
-
-        var banner = "";
-
-        if(css){
-            banner += '@charset "UTF-8";\n';
-        }
-
-        banner += '/*!\n' +
-        ' * '+name+version+' - <%=grunt.template.today("yyyy-mm-dd HH:MM")%>\n' +
-        ' * http://www.muenchen.de/\n' +
-        ' * Copyright (c) <%=grunt.template.today("yyyy")%> Portal München Betriebs-GmbH & Co. KG\n'+
-        ' */';
-
-        return banner;
-    };
-
-    /**
-     * Dynamicalle configure compass tasks based on sassEntryFiles
-     * @returns {{}}
-         */
-    var compassConfig = function(){
-
-        var config = {};
-        var filename;
-
-        for(var i = 0; i < sassEntryFiles.length; ++i){
-            filename = sassEntryFiles[i].filename;
-
-            config[filename] = {
-                options: {
-                    banner: bannerTemplate({name: sassEntryFiles[i].title, version: pkg.version, css:true}),
-                    specify: [ path.join( paths.sass, filename + '.scss') ],
-                    config: styleFiles.config,
-                    cssDir: paths.build.css,
-                    sassDir: paths.sass,
-                    imagesDir: paths.images,
-                    outputStyle: "expanded",
-                    force: true
-                    }
-            };
-
-        }
-
-        return config;
-    };
-
-    /**
+    /*
      * Dynamically generated array of all *.css => *.min.css files
-     * @returns {{}}
-             */
+     */
     var cssminFiles = function(){
         var files = {};
 
@@ -194,7 +105,14 @@ module.exports = function(grunt) {
                         expand: true,
                         cwd: paths.images,
                         src: '**/*',
-                        dest: paths.build.images,
+                        dest: paths.dist.images,
+                        filter: 'isFile'
+                    },
+                    {
+                        expand: true,
+                        cwd: paths.images,
+                        src: '**/*',
+                        dest: paths.dev.images,
                         filter: 'isFile'
                     }
                 ]
@@ -207,17 +125,16 @@ module.exports = function(grunt) {
                     debug: true
                 }
             },
-            script: {
+            scriptDev: {
                 src: jsFiles.script,
-                dest: jsFiles.build.scriptDev,
+                dest: jsFiles.dev.script,
                 options: {
                     plugin: ['tsify']
                 }
             },
-
-            search: {
-                src: jsFiles.search,
-                dest: jsFiles.build.searchDev,
+            script: {
+                src: jsFiles.script,
+                dest: jsFiles.dist.script,
                 options: {
                     plugin: ['tsify']
                 }
@@ -263,7 +180,9 @@ module.exports = function(grunt) {
          * Compass Task
          * Documentation: https://github.com/gruntjs/grunt-contrib-compass
          */
-        compass: compassConfig(),
+        sass: {
+
+        },
 
         cssmin: {
             target: {
@@ -282,13 +201,13 @@ module.exports = function(grunt) {
             ts: {
                 options: { cwd: paths.js },
                 files: [ "**/*.ts", "**/*.tsx" ],
-                tasks: [ "browserify:script" ]
+                tasks: [ "browserify:scriptDev" ]
             },
 
             css: {
                 options: { cwd: paths.sass },
                 files: [ "**/*" ],
-                tasks: [ "compass" ]
+                tasks: [ "sass" ]
             },
 
             images: {
@@ -301,8 +220,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.loadNpmTasks("grunt-contrib-compass");
-    grunt.loadNpmTasks("grunt-contrib-concat");
     grunt.loadNpmTasks("grunt-contrib-cssmin");
     grunt.loadNpmTasks("grunt-contrib-copy");
     grunt.loadNpmTasks("grunt-contrib-sass");
@@ -325,15 +242,6 @@ module.exports = function(grunt) {
         "scripts-min"
     ]);
 
-    grunt.registerTask("build-search", [
-        "browserify:search",
-        "uglify:search"
-    ]);
-
-    // Used during development
-    grunt.registerTask("test", function(){
-        grunt.log.writeln("Path: "+path.join("_sass","js"));
-    });
     /**
      * CSS: Production
      */
---
+++ sass/config.rb
@@ -0,0 +1,30 @@
+
+# Force encoding to UTF-8
+Encoding.default_external = 'utf-8'
+
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "_sass"
+images_dir = "css/images"
+javascripts_dir = "js"
+fonts_dir = "css/fonts"
+http_images_path = "images"
+
+output_style = :expanded
+environment = :development
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass _sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
